bc38c0b3b34b90b309cc3f6569d0b1eb51e7aa52
klubfisika <kf.teknis@gmail.com>
2025-06-02 11:46:06 +0700

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import qwikdev from '@qwikdev/astro';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), qwikdev()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Klub Fisika Indonesia';
+export const SITE_DESCRIPTION = 'Platform media sosial revolusioner untuk research, inovasi, dan kolaborasi ilmiah Indonesia!';
